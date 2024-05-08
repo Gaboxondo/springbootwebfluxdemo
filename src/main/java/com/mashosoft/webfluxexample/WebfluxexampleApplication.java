696b52a63a67b48240e929f9471ae8014cdef6d7
MSI\gabig <gabigarciagar@gmail.com>
2024-05-08 16:58:16 +0200

    first commit

---
+++ src/main/java/com/mashosoft/webfluxexample/WebfluxexampleApplication.java
@@ -0,0 +1,13 @@
+package com.mashosoft.webfluxexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mashosoft/webfluxexample/WebfluxexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashosoft.webfluxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
